e49ef51ae42e50e4398df77ec68b5029f49b9e1e
rabi12 <rabi.aranda@gmail.com>
2020-01-14 01:20:58 -0600

    Patron Singleton
    Practica

---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+
+    class Singleton
+    {
+        private static Singleton _instance; //Instancia privada
+
+        private Singleton() { } //Constructor privado
+
+        public static Singleton GetInstance() //Metodo para obtener la instancia
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        public static void someBusinessLogic()
+        {
+            // ...
+        }
+    }
+}
